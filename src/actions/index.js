3fa8c288e844ef5f5ef883a3f932c40fac8c0feb
mellaniesoriano <msoriano808@gmail.com>
2017-09-05 11:52:58 -1000

    fixed load and add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,23 @@
 /*jshint esversion:6*/
 
 export const LOAD_CARDS = 'LOAD_CARDS';
+export const ADD_CARD = 'ADD_CARD';
 export const DELETE_CARD = 'DELETE_CARD';
 export const UPDATE_CARD = 'UPDATE_CARD';
 
-export const loadCards = (card) => {
-  console.log('ACTION loading card: ', card);
+export const loadCards = (cards) => {
+  console.log('LOADING CARDS: ', cards);
   return {
     type : LOAD_CARDS,
-    card : card
+    cards : cards
+  };
+};
+
+export const addCard = (cards) => {
+  console.log('ACTION adding new card: ', cards);
+  return {
+    type : ADD_CARD,
+    cards : cards
   };
 };
 
