a7f4d2ab332a90649e44d47c68a42041a980309b
mellaniesoriano <msoriano808@gmail.com>
2017-09-05 11:57:08 -1000

    added redux-thunk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,9 @@
 /*jshint esversion:6*/
-import { ADD_USER, REMOVE_USER } from '../actions/Users.js';
+import { combineReducers } from 'redux';
+import cards from './cardReducers';
 
-const initialState = { users: [] };
+const reducers = combineReducers({
+  cards
+});
 
-const users = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_USER:
-      // return the schema of the state store
-      return {
-        users: [...state.users, action.users]
-      };
-
-    case REMOVE_USER:
-      return {
-        users: this.users.filter(users)
-      };
-
-    default:
-      return state;
-  }
-
-};
-
-export default users;
+export default reducers;
