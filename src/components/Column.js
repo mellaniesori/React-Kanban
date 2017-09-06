a9a14ea539e055acd1eb6477dcff90d46aa0eb84
mellaniesoriano <msoriano808@gmail.com>
2017-09-06 13:52:54 -1000

    created key to get rid of error

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,16 +1,23 @@
 /* jshint esversion:6 */
 import React from 'react';
-import KanbanCard from './../containers/KanbanCard';
+import KanbanCard from '../containers/KanbanCard';
 import 'bulma/css/bulma.css';
 
 const Column = ({ cards, columnName }) => {
   return (
-    <div className="columns">
+    <div className="column">
       <h2 className="column-title">
         { columnName }
       </h2>
         {
-          cards.map(card => <KanbanCard cards={ card } />)
+          cards
+          .map((card) => {
+            console.log('mapping', card)
+            return <KanbanCard
+            key={card.id} {...card}
+            />;
+
+          })
         }
   </div>
   )
