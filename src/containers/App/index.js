398ea51dcc8e1718239dab841c77e990dfc4225e
mellaniesoriano <msoriano808@gmail.com>
2017-09-06 20:41:51 -1000

    added header

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,16 @@
 /* jshint esversion:6 */
 import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
 import Board from '../Board';
 import CardForm from '../CardForm';
-import 'bulma/css/bulma.css';
+import Header from '../../components/Header';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="column">
+      <div className="container is-mobile">
+        <Header />
         <Board />
         <CardForm />
       </div>
