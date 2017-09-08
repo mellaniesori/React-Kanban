14739e4a2f838826863a95b024c3df7f8e86dc30
mellaniesoriano <msoriano808@gmail.com>
2017-09-07 17:54:19 -1000

    server setup and running

--- server/index.js
+++ server/index.js
@@ -1,71 +1,39 @@
-/*jshint esversion:6*/
 const express = require('express');
 const app = express();
+const bp = require('body-parser');
 const db = require('../models');
 const { Card } = db;
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
-app.get('/cards', (req, res)=> {
-  res.json({
-  "Queue": [
-    {
-      "_id": 1,
-      "title": "Make Better Styles",
-      "priority": "medium",
-      "status": "Queue",
-      "createdBy": "Ben",
-      "assignedTo": "Merlin"
-    },
-    {
-      "_id": 2,
-      "title": "Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles",
-      "priority": "low",
-      "status": "Queue",
-      "createdBy": "Ben",
-      "assignedTo": "Merlin"
-    },
-    {
-      "_id": 4,
-      "title": "Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles",
-      "priority": "high",
-      "status": "Queue",
-      "createdBy": "Ben",
-      "assignedTo": "Merlin"
-    },
-    {
-      "_id": 22,
-      "title": "Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles",
-      "priority": "blocker",
-      "status": "Queue",
-      "createdBy": "Ben",
-      "assignedTo": "Merlin"
-    }
-  ],
-  "in-progress": [
-    {
-      "_id": 7,
-      "title": "Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles",
-      "priority": "medium",
-      "status": "Progress",
-      "createdBy": "Ben",
-      "assignedTo": "Merlin"
-    }
-  ],
-  "done": [
-    {
-      "_id": 25,
-      "title": "Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles, Make Better Styles",
-      "priority": "Medium",
-      "status": "done",
-      "createdBy": "Ben",
-      "assignedTo": "Merlin"
-    }
-  ]
+// this file will handle DB
+
+app.use(bp.urlencoded());
+
+app.get('/', (req, res)=> {
+  Card.findAll()
+    .then((cards) => {
+      res.json(cards);
+    });
 });
+
+app.post('/cards', (req, res) => {
+  Card.create({
+    title: req.body.title,
+    priority: req.body.priority,
+    assignedTo: req.body.assignedTo,
+    createdBy: req.body.createdBy,
+    status: req.body.status
+  })
+  .then(() => {
+    Card.findAll()
+      .then((cards) => {
+        res.json(cards);
+      });
+  });
 });
 
 const server = app.listen(PORT, () => {
   db.sequelize.sync();
-  console.log(`Server funning on ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
