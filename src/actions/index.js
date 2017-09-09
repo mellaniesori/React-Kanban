93dbfeb83fa089a25a07ebea94cd43e8355cbede
mellaniesoriano <msoriano808@gmail.com>
2017-09-08 21:17:00 -1000

    delete and put working

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,58 +4,54 @@ import * as types from '../constants';
 import axios from "axios";
 var querystring = require('querystring');
 
-let nextCardId = 0;
-
 export const loadCards = (cards) => {
   return (dispatch) => {
     axios.get('/cards')
-    .then((cards) => {
-      dispatch({
-        type: types.LOAD_CARDS,
-        cards: cards.data
+      .then((cards) => {
+        dispatch({
+          type: types.LOAD_CARDS,
+          cards: cards.data
+        });
       });
-    });
   };
 };
 
 export const addCard = cards => {
   return (dispatch) => {
     axios.post('/cards', querystring.stringify(cards))
-    .then((cards) => {
-      console.log('add : ', cards)
-      dispatch({
-        type: types.ADD_CARD,
-        cards: cards.data
-      })
-    })
-  }
+      .then((cards) => {
+        console.log('add : ', cards);
+        dispatch({
+          type: types.LOAD_CARDS,
+          cards: cards.data
+        });
+      });
+  };
 };
 
 export const deleteCard = id => {
-  return {
-    type: types.DELETE_CARD,
-    id
+  let deleteID = parseInt(id, 10);
+  return (dispatch) => {
+    axios.delete(`/cards/${deleteID}/edit`)
+      .then((cards) => {
+        console.log('delete : ', cards);
+        dispatch({
+          type: types.LOAD_CARDS,
+          cards: cards.data
+        });
+      });
   };
 };
 
-export const updateCard = card => {
-  return {
-    type: types.UPDATE_CARD,
-    card
-  };
+export const updateCard = (id, edited) => {
+  let editId = parseInt(id, 10);
+    return (dispatch) => {
+      axios.put(`/cards/${editId}/edit`, querystring.stringify(edited))
+      .then((cards) => {
+        dispatch({
+          type: types.LOAD_CARDS,
+          cards: cards.data
+        });
+      });
+    };
 };
-
-
-// export const loadCards = () => {
-//   return (dispatch) => {
-//     new Promise((resolve, reject) => {
-//     var oReq = new XMLHttpRequest()
-//     oReq.addEventListener("load", function() {
-//       console.log(this)
-//       resolve(this.responseText)
-//     })
-//     oReq.open("GET", "/cards")
-//     oReq.send()
-//   })
-// }
-// }
